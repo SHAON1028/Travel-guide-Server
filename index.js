fe7e32744dc5e345675b036b8ead7c9e1e90f557
SHAON1028 <shaonkhairul@gmail.com>
2022-11-11 02:40:11 +0600

    sort date

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const express = require('express')
+const timestamp = require('time-stamp');
+const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -15,6 +17,23 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cvl7r98.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 // -------------------------------------------------------run
 
 async function run(){
@@ -23,7 +42,11 @@ async function run(){
         const serviceCollection = client.db('Travel').collection('services');
         const reviewCollection = client.db('Travel').collection('reviews');
 
-
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            res.send({token})
+        })  
         // operation
 
         app.get('/services', async (req, res) => {
@@ -50,12 +73,12 @@ async function run(){
         // review
         app.post('/reviews', async (req, res) => {
             const review = req.body;
-            const result = await reviewCollection.insertOne(review);
+            const result = await reviewCollection.insertOne(review)
             res.send(result);
         });
         app.get('/reviews', async (req, res) => {
             const query = {}
-            const cursor = reviewCollection.find(query);
+            const cursor = reviewCollection.find.sort({query,date:-1});
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
@@ -63,7 +86,7 @@ async function run(){
         app.get('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { reviewId: id };
-            const cursor = reviewCollection.find(query);
+            const cursor = reviewCollection.find.sort({query,date:-1});
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
