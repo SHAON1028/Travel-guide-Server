c65c286ea142a14ab69a5ef7839159b072cf2029
SHAON1028 <shaonkhairul@gmail.com>
2022-12-22 15:26:42 +0600

    review update fixed and cors policy fixed

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
+  }) 
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cvl7r98.mongodb.net/?retryWrites=true&w=majority`;
@@ -44,7 +49,7 @@ async function run(){
 
         app.post('/jwt', (req, res) =>{
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100d'})
             res.send({token})
         })  
         // operation
@@ -98,7 +103,7 @@ async function run(){
         app.get('/myreviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const review = await reviewCollection.findOne.sort(query);
+            const review = await reviewCollection.findOne(query);
             res.send(review);
         });
 
